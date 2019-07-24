a746ad80d21670726efe6bafebf0ff756726e63b
samuel grahame <samuel@intelogy.com.au>
2019-07-25 08:37:54 +1000

    Add project files.

---
+++ DotNetFramework2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetFramework2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
